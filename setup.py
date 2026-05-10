2fa1ec013cf0a810327eb952f60b55febe398b4f
Denis Pitzalis <denics@free.fr>
2026-05-10 13:10:35 +0200

    Fix: Update entry point to opm.skill for OPM compatibility

--- setup.py
+++ setup.py
@@ -81,5 +81,5 @@ setup(
     package_data={SKILL_PKG: find_resource_files()},
     include_package_data=True,
     keywords="ovos skill pokemon battle pokeapi",
-    entry_points={"ovos.plugin.skill": PLUGIN_ENTRY_POINT},
+    entry_points={"opm.skill": PLUGIN_ENTRY_POINT},
 )
